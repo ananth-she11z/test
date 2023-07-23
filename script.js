3c07b37c46578598b71e2b6fc4fdacf177390e34
she11z <ananth.venk88@gmail.com>
2023-07-23 18:01:49 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+fetch("data.json")
+.then(function(response){
+  return response.json();
+})
+.then(function(data){
+  let placeholder = document.querySelector("#data-output");
+  let out = "";
+  for(let data of data){
 
+    out += `
+      <tr>
+        <td>data.name</td>
+        <td>data.age</td>
+        <td>data.country</td>
+      </tr>
+    `;
+  }
+  placeholder.innerHTML = out;
+})
